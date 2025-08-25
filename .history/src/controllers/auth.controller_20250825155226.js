8d277951eeafa0904c09519fb158993516f8cb79
Rohit Rana <rkrana6631@gmail.com>
2025-08-25 15:55:39 +0530

    update

--- .history/src/controllers/auth.controller_20250825155226.js
+++ .history/src/controllers/auth.controller_20250825155226.js
@@ -646,7 +646,7 @@ exports.sendOtp = async (req, res) => {
 
     // Generate OTP
     const otp = Math.floor(1000 + Math.random() * 9000).toString();
-  
+    const otp = "1234";
 
     if (user) {
       // User exists, update OTP
@@ -833,8 +833,8 @@ exports.resendOtp = async (req, res) => {
     }
 
     // Generate new OTP
-    const newOtp = Math.floor(1000 + Math.random() * 9000).toString();
-    
+    // const newOtp = Math.floor(1000 + Math.random() * 9000).toString();
+     const newOtp = "1234";
     
     user.otp = newOtp;
     await user.save();
